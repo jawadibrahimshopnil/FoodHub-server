cc7738f16355d07c7c1b6585584f0042f571fdbb
jawadibrahim <jawadibrahimshopnil01@gmail.com>
2026-03-10 07:28:51 +0600

    fix: changed vercel config

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
 import app from './app';
 import config from './config';
 
-async function main() {
-  try {
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
-  } catch (err) {
-    console.log(err);
+const env = process.env.NODE_ENV;
+
+if(env === 'development'){
+  async function main() {
+    try {
+      app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`);
+      });
+    } catch (err) {
+        console.log(err);
+      }
   }
+
+  main();
 }
 
-main();
+export default app;
+
+
